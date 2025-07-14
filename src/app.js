bff56b741a8b2d11dbeb1fdcf42b94ab2a9f6a07
Huzefa Zaheer <huzefazaheer1@gmail.com>
2025-07-14 16:24:10 +0500

    Added msg detail view

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require("express")
 const path = require("path")
 const app = express()
 
-const PORT = 8080
+const PORT = process.env.PORT || 8080
 
 const publicPath = path.join(__dirname, "public")
 app.use(express.static(publicPath))
@@ -14,14 +14,17 @@ app.set("view engine", "ejs")
 // Get form data in req.body
 app.use(express.urlencoded({ extended: true }));
 
+let freeId = 3
 
 const messages = [
   {
+    id:1,
     text: "Hi there!",
     user: "Amando",
     added: new Date()
   },
   {
+    id:2,
     text: "Hello World!",
     user: "Charles",
     added: new Date()
@@ -33,17 +36,22 @@ app.get("/", (req, res) => {
     res.render('index', {messages:messages})
 })
 
+app.get("/messages/:id", (req, res) => {
+   res.render('messagedetail', {msg:messages[req.params.id-1]})
+})
+
 app.get("/new", (req, res) => {
     res.render('new')
 })
 
 app.post("/new", (req, res) => {
     messages.push({
+        id:freeId,
         text: req.body.msg,
         user: req.body.name,
         added: new Date()
         })
-    console.log(messages)
+    freeId++
     res.redirect("/")
 })
 
