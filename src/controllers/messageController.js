3a33bab3b04dc88956c7da83bdf7657d311e48d5
Huzefa Zaheer <huzefazaheer1@gmail.com>
2025-07-17 18:16:18 +0500

    Updated styelsheet

--- src/controllers/messageController.js
+++ src/controllers/messageController.js
@@ -1,8 +1,21 @@
 const { getMessages, getMessageById } = require("../models/db");
 
+function formatDateToYYYYMMDD(date) {
+  const year = date.getFullYear();
+  // Month is 0-indexed, so add 1
+  const month = (date.getMonth() + 1).toString().padStart(2, '0');
+  const day = date.getDate().toString().padStart(2, '0');
+
+  return `${year}-${month}-${day}`;
+}
+
 async function messageGet(req, res) {
     const msgs = await getMessages()
-    res.render("index", {messages: msgs})
+    const fmsgs = msgs.map(msg => ({
+        ...msg, // Keep all other properties
+        added: formatDateToYYYYMMDD(new Date(msg.added)) // Convert to Date object, then format
+    }));
+    res.render("index", {messages: fmsgs})
 }
 
 async function messageDetailGet(req, res) {
