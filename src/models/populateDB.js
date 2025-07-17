863bd6f1a34a189f17b9f2435994fa6f3ad45e88
Huzefa Zaheer <huzefazaheer1@gmail.com>
2025-07-17 17:33:22 +0500

    Added example data script for db

---
+++ src/models/populateDB.js
@@ -0,0 +1,31 @@
+#! /usr/bin/env node
+
+const { Client } = require("pg");
+
+const SQL = `
+CREATE TABLE IF NOT EXISTS messages (
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  text TEXT,
+  username VARCHAR ( 255 ),
+  added DATE
+);
+
+INSERT INTO messages (text, username, added) 
+VALUES
+  ('Hello World', 'Afiy', '2025-07-17'),
+  ('This is amazing', 'Afiys #1 Fan', '2025-07-20'),
+  ('This straight garbage', 'Afiys #1 Hater', '2025-07-20');
+`;
+
+async function main() {
+  console.log("seeding...");
+  const client = new Client({
+    connectionString: "postgresql://huzefa:123@localhost:5432/messageboard",
+  });
+  await client.connect();
+  await client.query(SQL);
+  await client.end();
+  console.log("done");
+}
+
+main();
